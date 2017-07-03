58968b812572393d117a2d5bf5d35f6378e0ec68
Xing Li <msuli_1120@ymail.com>
2017-07-02 23:30:15 -0700

    display data from firebase

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>Vuejs Firebase Application</h1>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Books List</h3>
+      </div>
+      <div class="panel-body">
+        <table class="table table-stripe">
+          <thead>
+            <tr>
+              <th>
+                Title
+              </th>
+              <th>
+                Author
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>
+                <a v-bind:href="book.url">{{book.title}}</a>
+              </td>
+              <td>
+                {{book.author}}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import Hello from './components/Hello'
   import Firebase from 'firebase'
 
   let config = {
@@ -14,7 +44,7 @@
     databaseURL: 'https://vuejs-a913f.firebaseio.com',
     projectId: 'vuejs-a913f',
     storageBucket: 'vuejs-a913f.appspot.com',
-    messagingSenderId: '147251775436',
+    messagingSenderId: '147251775436'
   }
 
   let app = Firebase.initializeApp(config)
@@ -25,7 +55,7 @@
   export default {
     name: 'app',
     firebase: {
-     books: bookRef
+      books: bookRef
     }
   }
 </script>
@@ -35,7 +65,6 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
