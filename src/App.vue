e59d98e5753564910018c4f974e055b0035a44f8
Xing Li <msuli_1120@ymail.com>
2017-07-02 23:11:05 -0700

    base frame

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+  import Hello from './components/Hello'
+  import Firebase from 'firebase'
+
+  let config = {
+    apiKey: 'AIzaSyBIjfO-0CUe6s5xJTeiAwQ27JbGxv6eaSg',
+    authDomain: 'vuejs-a913f.firebaseapp.com',
+    databaseURL: 'https://vuejs-a913f.firebaseio.com',
+    projectId: 'vuejs-a913f',
+    storageBucket: 'vuejs-a913f.appspot.com',
+    messagingSenderId: '147251775436',
+  }
+
+  let app = Firebase.initializeApp(config)
+  let db = app.database()
+
+  let bookRef = db.ref('books')
+
+  export default {
+    name: 'app',
+    firebase: {
+     books: bookRef
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
