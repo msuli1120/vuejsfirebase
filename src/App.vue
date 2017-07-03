667d9deb422dbec70dc52fde4a1043dd1564d41f
Xing Li <msuli_1120@ymail.com>
2017-07-02 23:40:59 -0700

    add form

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,30 @@
     <div class="page-header">
       <h1>Vuejs Firebase Application</h1>
     </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3>Add Book</h3>
+      </div>
+      <div class="panel-body">
+        <form class="form-inline" id="form">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3>Books List</h3>
@@ -56,6 +80,16 @@
     name: 'app',
     firebase: {
       books: bookRef
+    },
+    data () {
+      return {
+        newBook: {
+          title: '',
+          author: '',
+          url: ''
+        }
+
+      }
     }
   }
 </script>
